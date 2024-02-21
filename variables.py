23694488db229769732ea8db7d084fbc7dd5cf41
Danselo <danselo185@gmail.com>
2024-02-20 19:25:06 -0500

    First Step, Creation variables and concatenate with boolean and int or float

---
+++ variables.py
@@ -0,0 +1,17 @@
+#Create variables
+#Types
+first_name = 'Danilo'
+last_name = 'Lora'
+age = 21
+married = True
+complete_name = first_name + " " + last_name
+print(complete_name)
+print("I am " + str(age) + " years old");
+
+#create multiple variables
+name2,age2,married2 = 'Saide',19, True
+print("The name is: "+name2 + ' and age is: ' + str(age2) + " and married is " + str(married2))
+
+#Create multiples variables with the same value
+Leonardo = Patricio = Eduardo = 30
+print(Leonardo)
