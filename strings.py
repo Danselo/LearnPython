6cea374e5deeb31b8dade5f4aec1fa0bb7db248d
Danselo <danselo185@gmail.com>
2024-02-20 19:59:05 -0500

    add methods for strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+# String methods
+name = "saidee"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.count("e"))
+print(name.capitalize())
+print(name.replace("a", "o"))
+print(name*3)
+print(name.isdigit())
+print(name.isalpha())
--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-#Create variables
-#Types
+# Create variables
+# Types
 first_name = 'Danilo'
 last_name = 'Lora'
 age = 21
@@ -8,10 +8,10 @@ complete_name = first_name + " " + last_name
 print(complete_name)
 print("I am " + str(age) + " years old");
 
-#create multiple variables
-name2,age2,married2 = 'Saide',19, True
+# create multiple variables
+name2, age2, married2 = 'Saide', 19, True
 print("The name is: "+name2 + ' and age is: ' + str(age2) + " and married is " + str(married2))
 
-#Create multiples variables with the same value
+# Create multiples variables with the same value
 Leonardo = Patricio = Eduardo = 30
 print(Leonardo)
